ffe01c31f245b87b0d6439a00c2db1db1abc1845
SaintSkeeta <sholmesby@gmail.com>
2014-10-03 08:23:48 -0400

    Updated MVC controls to match package
    Matches the controls in the solution to the code in the distributed
    Sitecore package for the LaunchSitecore site.

--- Source/LaunchSitecoreMvc/Controllers/AccountController.cs
+++ Source/LaunchSitecoreMvc/Controllers/AccountController.cs
@@ -9,14 +9,10 @@ using LaunchSitecore.Models;
 using LaunchSitecore.Configuration.SiteUI.Analytics;
 using Sitecore.Analytics;
 using LaunchSitecore.Configuration.SiteUI.Base;
-using Sitecore.Analytics.Model.Entities;
-using Sitecore.Analytics.Tracking;
-using Sitecore.Diagnostics;
 
 namespace LaunchSitecore.Controllers
 {
   [Authorize]
-  //[InitializeSimpleMembership]
   public class AccountController : LaunchSitecoreBaseController 
   {
     //
@@ -32,7 +28,6 @@ namespace LaunchSitecore.Controllers
     // POST: /Account/Login
     [HttpPost]
     [AllowAnonymous]
-    [ValidateAntiForgeryToken]
     public ActionResult Login(LoginModel model, string returnUrl)
     {
       if (ModelState.IsValid)
@@ -41,12 +36,9 @@ namespace LaunchSitecore.Controllers
         string domainUser = domain.Name + @"\" + model.UserName;
         if (Sitecore.Security.Authentication.AuthenticationManager.Login(domainUser, model.Password, model.RememberMe))
         {
-          // identify the user (Username is Email)
-          Tracker.Current.Session.Identify(model.UserName);
-
           // Register Goal & set a few values in the visit tags.
-          Tracker.Current.CurrentPage.Register("Login", "[Login] Username: \"" + domainUser + "\"");
-          AnalyticsHelper.SetVisitTagsOnLogin(domainUser);
+          AnalyticsHelper.RegisterGoalOnCurrentPage("Login", "[Login] Username: \"" + domainUser + "\"");
+          AnalyticsHelper.SetVisitTagsOnLogin(domainUser, false);
           return RedirectToLocal(returnUrl);
         }
       }
@@ -59,15 +51,20 @@ namespace LaunchSitecore.Controllers
     //
     // POST: /Account/LogOff
     [HttpPost]
-    [ValidateAntiForgeryToken]
     public ActionResult LogOff()
     {
+      //throw new Exception("Hello there");
       Sitecore.Security.Authentication.AuthenticationManager.Logout();
-      
-      // calling Session Abandon flushes the session data out to the xDB
+	  
+	  // calling Session Abandon flushes the session data out to the xDB
       Session.Abandon();
+	  
       Sitecore.Web.WebUtil.Redirect("/");
       return null;
+
+      // By default in Launch Sitecore we return the user the home page on logout.  It is important to understand how the Session Provider works in 7.5+ though.
+      // The Sitecore session provider pushes the session data to the xDB on session end not logout.  
+      // If you want to force this is development environemnts, you can call Session.Abandon() instead of the redirect.
     }
 
     //
@@ -82,7 +79,6 @@ namespace LaunchSitecore.Controllers
     // POST: /Account/Register
     [HttpPost]
     [AllowAnonymous]
-    [ValidateAntiForgeryToken]
     public ActionResult Register(RegisterModel model)
     {
       if (ModelState.IsValid)
@@ -103,40 +99,8 @@ namespace LaunchSitecore.Controllers
             Sitecore.Context.User.Profile.ProfileItemId = "{93B42F5F-17A9-441B-AB6D-444F714EF384}"; //LS User
             Sitecore.Context.User.Profile.Save();
 
-            // identify the user (which should add them)
-            Tracker.Current.Session.Identify(model.Email);
-
-            Contact contact = Sitecore.Analytics.Tracker.Current.Contact;
-            try
-            {
-                // set the personal information for the contact
-                IContactPersonalInfo personalInfo = contact.GetFacet<IContactPersonalInfo>("Personal");
-                personalInfo.FirstName = model.FullName.Split(' ')[0];
-                personalInfo.Surname = model.FullName.Split(' ')[1];
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Error setting the User's Personal Info: " + ex.ToString(), this);
-            }
-
-            try
-            {
-                // set the email address
-                IContactEmailAddresses emailAddresses = contact.GetFacet<IContactEmailAddresses>("Emails");
-                emailAddresses.Preferred = model.Email;
-
-                if (!emailAddresses.Entries.Contains(model.Email))
-                {
-                    emailAddresses.Entries.Create(model.Email);
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Error assigning email address: " + ex.ToString(), this);
-            }
-
-            Tracker.Current.CurrentPage.Register("Register", "[Register] Username: \"" + domainUser + "\"");
-            AnalyticsHelper.SetVisitTagsOnLogin(domainUser);
+            AnalyticsHelper.RegisterGoalOnCurrentPage("Register", "[Register] Username: \"" + domainUser + "\"");
+            AnalyticsHelper.SetVisitTagsOnLogin(domainUser, true);
             Sitecore.Web.WebUtil.Redirect("/");
           }
         }
--- Source/LaunchSitecoreMvc/Models/VisitInformation.cs
+++ Source/LaunchSitecoreMvc/Models/VisitInformation.cs
@@ -1,6 +1,5 @@
 ﻿using LaunchSitecore.Configuration;
 using Sitecore.Analytics;
-using Sitecore.Analytics.Core;
 using Sitecore.Analytics.Tracking;
 using Sitecore.Data;
 using Sitecore.Data.Fields;
@@ -13,149 +12,146 @@ using System.Web;
 
 namespace LaunchSitecore.Models
 {
-  public class VisitInformation
+ public class VisitInformation
+ {
+  public string PageCount
   {
-    public string PageCount
-    {
-      get { return Convert.ToString(Tracker.Current.Interaction.PageCount); }
-    }
-
-    public string EngagementValue
-    {
-      get { return Convert.ToString(Tracker.Current.Interaction.Value); }
-    }
-
-    public string Campaign
-    {
-      get
-      { 
-        // the call to Tracker.CurrentVisit.CampaignId will either work or throw
-        try
-        {
-          Item campaign = Sitecore.Context.Database.GetItem(new ID(Tracker.Current.Interaction.CampaignId.Value));
-          return campaign.Name;
-        }
-        catch { return SiteConfiguration.GetDictionaryText("Current Campaign Empty"); }
-      }
-    }
-
-    public string City
-    {
-      get { return Tracker.Current.Interaction.HasGeoIpData ? Tracker.Current.Interaction.GeoData.City : SiteConfiguration.GetDictionaryText("Pending Lookup"); }
-    }
-
-    public string PostalCode
-    {
-      get { return Tracker.Current.Interaction.HasGeoIpData ? Tracker.Current.Interaction.GeoData.PostalCode : SiteConfiguration.GetDictionaryText("Pending Lookup"); }
-    }
-
-    public List<PatternMatch> PatternMatches { get { return LoadPatterns(); } }
-
-    public List<GenericLink> PagesViewed { get { return LoadPages(); } }
-
-    public List<PatternMatch> LoadPatterns()
-    {
-      List<PatternMatch> patternMatches = new List<PatternMatch>();
-
-      if (SiteConfiguration.GetSiteSettingsItem() != null)
-      {
-        MultilistField profiles = SiteConfiguration.GetSiteSettingsItem().Fields["Visible Profiles"];
-        foreach (Item vp in profiles.GetItems())
-        {
-          Item evaluatorTypeProfile = Sitecore.Context.Database.GetItem(vp.Paths.FullPath);
-          if (evaluatorTypeProfile != null)
-          {
-            if (Sitecore.Analytics.Tracker.IsActive)
-            {
-              // show the pattern match if there is one.
-              if (Tracker.Current.Interaction.Profiles.ContainsProfile(evaluatorTypeProfile.Name))
-              {
-                Profile personaProfile = Tracker.Current.Interaction.Profiles[evaluatorTypeProfile.Name];
-
-                // load the details about the matching pattern
-                if (personaProfile.PatternId.HasValue)
-                {
-                    Item i = Sitecore.Context.Database.GetItem(new ID(personaProfile.PatternId.Value));
-                    if (i != null)
-                    {
-                        Sitecore.Data.Items.MediaItem image = new Sitecore.Data.Items.MediaItem(((ImageField)i.Fields["Image"]).MediaItem);
-                        string src = Sitecore.StringUtil.EnsurePrefix('/', Sitecore.Resources.Media.MediaManager.GetMediaUrl(image));
-                        patternMatches.Add(new PatternMatch(evaluatorTypeProfile["Name"], i.Name, src));
-                    }
-                }
-              }
-            }
-          }
-        }
-      }
-      return patternMatches;
-    }
-
-    public List<GenericLink> LoadPages()
-    {
-      List<GenericLink> pagesViewed = new List<GenericLink>();
-
-      foreach (Page page in Tracker.Current.Interaction.Pages)
-      {
-        GenericLink link = new GenericLink(CleanPageName(page), page.Url.ToString(), false);
-        pagesViewed.Add(link);
-      }
-      pagesViewed.Reverse();
-      return pagesViewed;
-    }
-
-    private string CleanPageName(Page p)
-    {
-      string pageName = p.Url.ToString().Replace("/en", "/").Replace("//", "/").Remove(0, 1).Replace(".aspx", "");
-      if (pageName == String.Empty || pageName == "en") pageName = "Home";
-      if (pageName.IndexOf("/") != pageName.LastIndexOf("/"))
-      {
-        pageName = pageName.Substring(0, pageName.IndexOf("/") + 1) + "..." + pageName.Substring(pageName.LastIndexOf("/"));
-      }
-      return (pageName.Length < 27) ? String.Format("{0} ({1}s)", pageName, (p.Duration / 1000.0).ToString("f2")) :
-          String.Format("{0}... ({1}s)", pageName.Substring(0, 26), (p.Duration / 1000.0).ToString("f2"));     
-    }
-
-    private string CleanGoalDescription(Sitecore.Analytics.Data.Items.PageEventItem a)
-    {
-        return String.Format("{0} ({1})", a.Name, a.Points);
-    }
-
-    protected void FakeIPForLocalhost()
-    {
-        Sitecore.Analytics.Tracking.CurrentInteraction currentVisit = Tracker.Current.Interaction;
-        if (currentVisit != null)
-        {
-            // if we are local host. our IP is 127 which will not resolve so I am using a 'fake' ip address
-            if (currentVisit.Ip[0] == 127)
-            {
-                currentVisit.Ip[0] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP1"]);
-                currentVisit.Ip[1] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP2"]);
-                currentVisit.Ip[2] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP3"]);
-                currentVisit.Ip[3] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP4"]);
-
-                // Sitecore may have already tried to resolve the 127 and failed, so this will initiate a retry
-                //currentVisit.HasGeoIpData = false;
-
-                // Save our changes and let DMS request the GeoIP data again.  
-                currentVisit.UpdateGeoIpData(new TimeSpan(0, 0, 0, 0, 100));
-                //currentVisit.AcceptChanges();
-            }
-        }
-    }
+   get { return Convert.ToString(Tracker.Current.Interaction.PageCount); }
   }
 
-  public class PatternMatch 
+  public string EngagementValue
   {
-    public PatternMatch() { }
-    public PatternMatch(string profile, string pattern, string image) 
+   get { return Convert.ToString(Tracker.Current.Interaction.Value); }
+  }
+
+  public string Campaign
+  {
+   get
+   {
+    // the call to Tracker.CurrentVisit.CampaignId will either work or throw       
+    try
     {
-      Profile = profile;
-      PatternName = pattern;
-      Image = image;
+     if (Tracker.Current.Interaction.CampaignId.HasValue)
+     {
+      Item campaign = Sitecore.Context.Database.GetItem(new ID(Tracker.Current.Interaction.CampaignId.Value));
+      return campaign.Name;
+     }
     }
-    public string Profile { get; set; }
-    public string PatternName { get; set; } 
-    public string Image { get; set; }
+    catch { }
+
+    return SiteConfiguration.GetDictionaryText("Current Campaign Empty");
+   }
+  }
+
+  public string City
+  {
+   get { return Tracker.Current.Interaction.HasGeoIpData ? Tracker.Current.Interaction.GeoData.City : SiteConfiguration.GetDictionaryText("Pending Lookup"); }
+  }
+
+  public string PostalCode
+  {
+   get { return Tracker.Current.Interaction.HasGeoIpData ? Tracker.Current.Interaction.GeoData.PostalCode : SiteConfiguration.GetDictionaryText("Pending Lookup"); }
+  }
+
+  public List<PatternMatch> PatternMatches { get { return LoadPatterns(); } }
+
+  public List<GenericLink> PagesViewed { get { return LoadPages(); } }
+
+  public List<PatternMatch> LoadPatterns()
+  {
+   List<PatternMatch> patternMatches = new List<PatternMatch>();
+   
+   if (Tracker.IsActive)
+   {
+    if (SiteConfiguration.GetSiteSettingsItem() != null)
+    {
+     MultilistField profiles = SiteConfiguration.GetSiteSettingsItem().Fields["Visible Profiles"];
+     foreach (Item visibleProfile in profiles.GetItems())
+     {
+      Item visibleProfileItem = Sitecore.Context.Database.GetItem(visibleProfile.ID);
+      if (visibleProfileItem != null)
+      {      
+       // show the pattern match if there is one.
+       var userPattern = Tracker.Current.Interaction.Profiles[visibleProfileItem.Name];
+       if (userPattern != null)
+       {
+        // load the details about the matching pattern
+        Item matchingPattern = Sitecore.Context.Database.GetItem(userPattern.PatternId.ToId());
+        if (matchingPattern != null)
+        {
+         Sitecore.Data.Items.MediaItem image = new Sitecore.Data.Items.MediaItem(((ImageField) matchingPattern.Fields["Image"]).MediaItem);
+         string src = Sitecore.StringUtil.EnsurePrefix('/', Sitecore.Resources.Media.MediaManager.GetMediaUrl(image));
+         patternMatches.Add(new PatternMatch(visibleProfileItem["Name"], matchingPattern.Name, src));
+        }
+       }
+      }
+     }
+    }
+   }
+   return patternMatches;
+  }
+
+  public List<GenericLink> LoadPages()
+  {
+   List<GenericLink> pagesViewed = new List<GenericLink>();
+
+   foreach (IPageContext page in Tracker.Current.Interaction.GetPages())
+   {
+    GenericLink link = new GenericLink(CleanPageName(page), page.Url.Path, false);
+    pagesViewed.Add(link);
+   }
+   pagesViewed.Reverse();
+   return pagesViewed;
+  }
+
+  private string CleanPageName(IPageContext p)
+  {
+   string pageName = p.Url.Path.Replace("/en", "/").Replace("//", "/").Remove(0, 1).Replace(".aspx", "");
+   if (pageName == String.Empty || pageName == "en") pageName = "Home";
+   if (pageName.IndexOf("/") != pageName.LastIndexOf("/"))
+   {
+    pageName = pageName.Substring(0, pageName.IndexOf("/") + 1) + "..." + pageName.Substring(pageName.LastIndexOf("/"));
+   }
+   return (pageName.Length < 27) ? String.Format("{0} ({1}s)", pageName, (p.Duration / 1000.0).ToString("f2")) :
+       String.Format("{0}... ({1}s)", pageName.Substring(0, 26), (p.Duration / 1000.0).ToString("f2"));
+  }
+
+  protected void FakeIPForLocalhost()
+  {
+   var ci = Tracker.Current.Interaction;
+   if (ci != null)
+   {
+    // if we are local host. our IP is 127 which will not resolve so I am using a 'fake' ip address
+    if (ci.Ip[0] == 127)
+    {
+     ci.Ip[0] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP1"]);
+     ci.Ip[1] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP2"]);
+     ci.Ip[2] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP3"]);
+     ci.Ip[3] = Convert.ToByte(SiteConfiguration.GetSiteSettingsItem()["IP4"]);
+
+     // Sitecore may have already tried to resolve the 127 and failed, so this will initiate a retry
+     //ci.SetGeoData(null);
+
+     // Save our changes and let DMS request the GeoIP data again.  
+     ci.UpdateGeoIpData(new TimeSpan(0, 0, 0, 0, 100));
+     ci.AcceptModifications();
+    }
+   }
+  }
+ }
+
+
+  public class PatternMatch
+  {
+   public PatternMatch() { }
+   public PatternMatch(string profile, string pattern, string image)
+   {
+    Profile = profile;
+    PatternName = pattern;
+    Image = image;
+   }
+   public string Profile { get; set; }
+   public string PatternName { get; set; }
+   public string Image { get; set; }
   }
 }
