893f749c725ea72cf69730508628c7c8e2cd1bb2
SaintSkeeta <sholmesby@gmail.com>
2014-12-20 17:25:44 -0500

    Temp fix for Experience Explorer in SC8
    Issue is with checking the User Profile.

--- Source/LaunchSitecore.Client/layouts/LaunchSitecore/Controls/Navigation/Tertiary Nav.ascx.cs
+++ Source/LaunchSitecore.Client/layouts/LaunchSitecore/Controls/Navigation/Tertiary Nav.ascx.cs
@@ -56,16 +56,20 @@ namespace LaunchSitecore.layouts.LaunchSitecore.Controls.Navigation
     private void LoadFavorites()
     {
       List<Item> items = new List<Item>();
-      Sitecore.Security.Accounts.User user = Sitecore.Context.User;
-      Sitecore.Security.UserProfile profile = user.Profile;
-      string ItemIds = profile.GetCustomProperty("Favorites");
 
-      foreach (string itemId in ItemIds.Split('|'))
-      {
-        Item item = Sitecore.Context.Database.GetItem(itemId);
-        if (item != null)
-          items.Add(item);
-      }
+          Sitecore.Security.Accounts.User user = Sitecore.Context.User;
+          if (user.Name == "sitecore\\eevirtualuser")
+              return;
+
+          Sitecore.Security.UserProfile profile = user.Profile;
+          string ItemIds = profile.GetCustomProperty("Favorites");
+
+          foreach (string itemId in ItemIds.Split('|'))
+          {
+              Item item = Sitecore.Context.Database.GetItem(itemId);
+              if (item != null)
+                  items.Add(item);
+          }
 
       if (items.Count > 0)
       {
