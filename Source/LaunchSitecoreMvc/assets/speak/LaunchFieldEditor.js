3a0f4e2362a42156ef524dc2940270d596c5f2fc
SaintSkeeta <sholmesby@gmail.com>
2015-10-23 22:07:32 -0400

    Updated files to match the 8124 package.

--- Source/LaunchSitecoreMvc/assets/speak/LaunchFieldEditor.js
+++ Source/LaunchSitecoreMvc/assets/speak/LaunchFieldEditor.js
@@ -1,21 +1,23 @@
-﻿define(["sitecore"], function (Sitecore) {
+﻿define(["sitecore",
+  "/-/speak/v1/ExperienceEditor/ExperienceEditor.js",
+  "/-/speak/v1/ExperienceEditor/ExperienceEditor.Context.js"],
+  function (Sitecore, ExperienceEditor, ExperienceEditorContext) {
+      Sitecore.Commands.LaunchFieldEditor =
+      {
+          canExecute: function (context) {
+              if (ExperienceEditor.isInMode("edit")) {
+                  return true;
+              }
+              return false;
+          },
+          execute: function (context) {
+              context.currentContext.argument = context.button.viewModel.$el[0].accessKey;
 
-    Sitecore.Commands.LaunchFieldEditor =
-    {
-        canExecute: function (context) {
-            if (!Sitecore.ExperienceEditor.isInMode("edit")) {
-                return false;
-            }
-            return true;
-        },
-        execute: function (context) {
-            context.currentContext.argument = context.button.viewModel.$el[0].accessKey;
-
-            Sitecore.ExperienceEditor.PipelinesUtil.generateRequestProcessor("ExperienceEditor.GenerateFieldEditorUrl", function (response) {
-                var DialogUrl = response.responseValue.value;
-                var dialogFeatures = "dialogHeight: 680px;dialogWidth: 520px;";
-                Sitecore.ExperienceEditor.Dialogs.showModalDialog(DialogUrl, '', dialogFeatures, null);
-            }).execute(context);
-        }
-    };
-});
+              ExperienceEditor.PipelinesUtil.generateRequestProcessor("ExperienceEditor.GenerateFieldEditorUrl", function (response) {
+                  var DialogUrl = response.responseValue.value;
+                  var dialogFeatures = "dialogHeight: 680px;dialogWidth: 520px;";
+                  ExperienceEditor.Dialogs.showModalDialog(DialogUrl, '', dialogFeatures, null);
+              }).execute(context);
+          }
+      };
+  });
