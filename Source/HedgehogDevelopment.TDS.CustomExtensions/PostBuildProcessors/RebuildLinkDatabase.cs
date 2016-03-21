214d74e49be4a9b7e8fb029d11e0777c37ac0d14
SaintSkeeta <sholmesby@gmail.com>
2016-03-20 21:21:42 -0400

    Added TDS v5.5 Post Deploy Actions.
     - Rebuild Search Indexes
     - Rebuild Link Database
     - Publish Master to Web
     - Added Update Package Creation (using Sitecore.Packaging.CoreGroup).

---
+++ Source/HedgehogDevelopment.TDS.CustomExtensions/PostBuildProcessors/RebuildLinkDatabase.cs
@@ -0,0 +1,42 @@
+﻿using HedgehogDevelopment.SitecoreProject.PackageInstallPostProcessor.Contracts;
+using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HedgehogDevelopment.TDS.CustomExtensions.PostBuildProcessors
+{
+    public class RebuildLinkDatabase : IPostDeployAction
+    {
+        public void RunPostDeployAction(XDocument deployedItems, IPostDeployActionHost host, string parameter)
+        {
+            var databases = this.GetDatabases(parameter);
+            foreach (string databaseName in databases)
+            {
+                Database database = Sitecore.Data.Database.GetDatabase(databaseName);
+                if (database == null)
+                {
+                    host.LogMessage("Database with the name '{0}' in null", databaseName);
+                    continue;
+                }
+                Sitecore.Globals.LinkDatabase.Rebuild(database);
+            }
+        }
+
+        private string[] GetDatabases(string parameters)
+        {
+            if (string.IsNullOrEmpty(parameters))
+            {
+                throw new System.InvalidOperationException("Please specify comma separated databases in the parameter setting.");
+            }
+
+            return parameters.Split(new char[]
+            {
+                ','
+            }, System.StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ Source/HedgehogDevelopment.TDS.CustomExtensions/PostBuildProcessors/SmartPublishMasterToWeb.cs
@@ -0,0 +1,27 @@
+﻿using HedgehogDevelopment.SitecoreProject.PackageInstallPostProcessor.Contracts;
+using Sitecore.Data;
+using Sitecore.Data.Managers;
+using Sitecore.Globalization;
+using Sitecore.Publishing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HedgehogDevelopment.TDS.CustomExtensions.PostBuildProcessors
+{
+    public class SmartPublishMasterToWeb : IPostDeployAction
+    {
+        public void RunPostDeployAction(XDocument deployedItems, IPostDeployActionHost host, string parameter)
+        {
+
+            host.LogMessage("Smart publishing master to web");
+            PublishManager.PublishSmart(Sitecore.Data.Database.GetDatabase("master"), 
+                                        new [] { Sitecore.Data.Database.GetDatabase("web") }, 
+                                        new [] { LanguageManager.GetLanguage("en") });
+
+        }
+    }
+}
