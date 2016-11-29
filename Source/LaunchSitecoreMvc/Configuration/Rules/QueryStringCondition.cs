ba9dd5370f384fb769857655056fef5b4027811c
Sean Holmesby <sholmesby@gmail.com>
2016-11-29 13:55:03 -0500

    Added custom QueryString rule for testing.

---
+++ Source/LaunchSitecoreMvc/Configuration/Rules/QueryStringCondition.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Rules.Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Rules;
+
+namespace LaunchSitecore.Configuration.Rules
+{
+    public class QueryStringCondition<T> : WhenCondition<T> where T : RuleContext
+    {
+        public string QueryStringKey { get; set; }
+        public string QueryStringValue { get; set; }
+
+        protected override bool Execute(T ruleContext)
+        {
+            string qs = HttpContext.Current.Request.QueryString[QueryStringKey];
+            if (qs == QueryStringValue)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
