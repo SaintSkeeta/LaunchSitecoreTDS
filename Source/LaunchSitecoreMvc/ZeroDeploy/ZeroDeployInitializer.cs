661f2388fd375edf551c19f6998fe0b22e35516d
cturano <cturano@LPTP-DEV026>
2016-12-13 11:52:29 -0500

    Add

---
+++ Source/LaunchSitecoreMvc/ZeroDeploy/ZeroDeployInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Hedgehog.ZeroDeploy.Client;
+using Hedgehog.ZeroDeploy.Glass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace LaunchSitecore.ZeroDeploy
+{
+    public class ZeroDeployInitializer : IZeroDeployInitializer
+    {
+        public void InitializeZeroDeploy()
+        {
+            ZeroDeployGlassHelpers.GlassZeroDeployInitialization();
+
+            ZeroDeployGlassHelpers.RegisterAssemblyWithGlass(Assembly.GetExecutingAssembly());
+        }
+    }
+}
