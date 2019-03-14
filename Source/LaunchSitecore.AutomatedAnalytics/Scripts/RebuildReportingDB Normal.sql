7c70c717dc626b8a07528d79b45d2754934c0093
Sean Holmesby <sholmesby@gmail.com>
2019-03-14 10:26:09 -0400

    Updated ReportingDB rebuild scripts to LaunchTest DB Prefixes.

--- Source/LaunchSitecore.AutomatedAnalytics/Scripts/RebuildReportingDB Normal.sql
+++ Source/LaunchSitecore.AutomatedAnalytics/Scripts/RebuildReportingDB Normal.sql
@@ -1,25 +1,25 @@
 -- Reporting to Secondary
 
-INSERT INTO [dhrdummy_reporting_secondary]..CampaignActivityDefinitions
-SELECT * FROM [dhrdummy_reporting]..CampaignActivityDefinitions
+INSERT INTO [LaunchTest_reporting_secondary]..CampaignActivityDefinitions
+SELECT * FROM [LaunchTest_reporting]..CampaignActivityDefinitions
 
-INSERT INTO [dhrdummy_reporting_secondary]..GoalDefinitions
-SELECT * FROM [dhrdummy_reporting]..GoalDefinitions
+INSERT INTO [LaunchTest_reporting_secondary]..GoalDefinitions
+SELECT * FROM [LaunchTest_reporting]..GoalDefinitions
  
-INSERT INTO [dhrdummy_reporting_secondary]..OutcomeDefinitions
-SELECT * FROM [dhrdummy_reporting]..OutcomeDefinitions
+INSERT INTO [LaunchTest_reporting_secondary]..OutcomeDefinitions
+SELECT * FROM [LaunchTest_reporting]..OutcomeDefinitions
  
-INSERT INTO [dhrdummy_reporting_secondary]..MarketingAssetDefinitions
-SELECT * FROM [dhrdummy_reporting]..MarketingAssetDefinitions
+INSERT INTO [LaunchTest_reporting_secondary]..MarketingAssetDefinitions
+SELECT * FROM [LaunchTest_reporting]..MarketingAssetDefinitions
 
-INSERT INTO [dhrdummy_reporting_secondary]..Segments
-SELECT * FROM [dhrdummy_reporting]..Segments
+INSERT INTO [LaunchTest_reporting_secondary]..Segments
+SELECT * FROM [LaunchTest_reporting]..Segments
  
-INSERT INTO [dhrdummy_reporting_secondary]..Taxonomy_TaxonEntity
-SELECT * FROM [dhrdummy_reporting]..Taxonomy_TaxonEntity
+INSERT INTO [LaunchTest_reporting_secondary]..Taxonomy_TaxonEntity
+SELECT * FROM [LaunchTest_reporting]..Taxonomy_TaxonEntity
  
-INSERT INTO [dhrdummy_reporting_secondary]..Taxonomy_TaxonEntityFieldDefinition
-SELECT * FROM [dhrdummy_reporting]..Taxonomy_TaxonEntityFieldDefinition
+INSERT INTO [LaunchTest_reporting_secondary]..Taxonomy_TaxonEntityFieldDefinition
+SELECT * FROM [LaunchTest_reporting]..Taxonomy_TaxonEntityFieldDefinition
  
-INSERT INTO [dhrdummy_reporting_secondary]..Taxonomy_TaxonEntityFieldValue
-SELECT * FROM [dhrdummy_reporting]..Taxonomy_TaxonEntityFieldValue
+INSERT INTO [LaunchTest_reporting_secondary]..Taxonomy_TaxonEntityFieldValue
+SELECT * FROM [LaunchTest_reporting]..Taxonomy_TaxonEntityFieldValue
