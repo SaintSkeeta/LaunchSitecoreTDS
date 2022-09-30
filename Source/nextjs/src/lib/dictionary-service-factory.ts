44277662179107b2f921bbe5e72e5b95502e87c0
Sean Holmesby <sholmesby@gmail.com>
2022-09-30 16:16:01 +1000

    Added the rootItemId to the dictionary service factory.

--- Source/nextjs/src/lib/dictionary-service-factory.ts
+++ Source/nextjs/src/lib/dictionary-service-factory.ts
@@ -12,6 +12,7 @@ export class DictionaryServiceFactory {
           endpoint: config.graphQLEndpoint,
           apiKey: config.sitecoreApiKey,
           siteName: config.jssAppName,
+          rootItemId: '{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}'
           /*
             The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
             app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
